5508e686b71132699a281086127fedf26293d001
snehalbhayani <snehal.bhayani@yahoo.com>
2015-05-06 13:34:06 +0530

    Update processor.py

--- grammarcheck/processor.py
+++ grammarcheck/processor.py
@@ -1,22 +1,40 @@
+import json
 import io
 import sys
 import language_check
 import rethinkdb as r
 from rethinkdb import RqlRuntimeError
 from plagiarismChecker import PlagiarismChecker
+import psycopg2
+from datetime import datetime
+
 class Processor:
 	
     def __init__(self):
         simple_dictionary=[]
         self.macros={}
 		
-
-
-
-    def start(self,pipe):
-        print ('Starting the Processor')
+   
+    def analyze_comments_from_db(self,pipe):
         self.pipe=pipe
-#Load the dictionary
+# Get the comments from the comments table. Currently all of the comments are pulled from the database. But, later on a date range is to be used for 
+# getting only the 'new' comments.
+        conn = psycopg2.connect("dbname=iwadmin user=idb password=idb")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM comments ORDER BY user_id;")
+        all_comments=cur.fetchall()
+        dict_of_comments_by_users={}
+        for comment in all_comments:
+            if comment[4] not in dict_of_comments_by_users.keys():
+                dict_of_comments_by_users[comment[4]]={}
+            if comment[2] not in dict_of_comments_by_users[comment[4]].keys():
+                dict_of_comments_by_users[comment[4]][comment[2]]=[]
+
+            dict_of_comments_by_users[comment[4]][comment[2]].append({'data':comment[1],'datetime':comment[5]})
+        cur.close()
+        conn.close()
+
+# Creating and/or updating the lagrammer database and the comments table in rethinkdb.
         r.connect('localhost', 28015).repl()
         tool=language_check.LanguageTool('en-GB')
         try:
@@ -25,8 +43,75 @@ class Processor:
             try:
                 r.db('lagrammer').table_create('comments').run()
             except RqlRuntimeError:
-                print("The table aready exists")            
-#        with open('comments','r') as comments:
+                print("The table aready exists")
+
+
+        users=dict_of_comments_by_users.keys()
+        pc = PlagiarismChecker()
+        dict_of_items={}
+        for user in users:
+            items=dict_of_comments_by_users[user].keys()
+            for item in items:
+                if (item not in dict_of_items):
+                    dict_of_items[item]={}
+                if (user not in dict_of_items[item]):
+                    dict_of_items[item][user]=[]
+                comments=dict_of_comments_by_users[user][item]    
+                for comment in comments:
+                    user_dict={}
+                    comment_dict={}
+                    comment_dict['user_id']=user
+                    comment_dict['item_id']=item
+                    comment_dict['data']=comment['data']
+                    comment_dict['datetimestamp']=str(comment['datetime'])
+                    print('The sentence is:'+comment['data'])
+                    users_by_item=dict_of_items[item].keys()
+                    for user_by_item in users_by_item:
+                        if comment['data'] == dict_of_items[item][user_by_item]:
+                            print('plagiarised')
+                            comment_dict['plagiarised']=True
+                            user_dict['comment']=comment_dict
+                            break
+                        if 'plagiarised' in comment_dict and comment_dict['plagiarised']==True :
+                            continue
+                    matches=tool.check(comment['data'])
+                    print('analyzed')
+                    analysis={'rule_id':[],'str':[],'category':[],'msg':[],'spos':[],'epos':[],'suggestions':[]}
+                    for match in matches:
+                        analysis['rule_id'].append(match.ruleId)
+                        analysis['str'].append(match.__str__())
+                        analysis['category'].append(match.category)
+                        analysis['msg'].append(match.msg)
+                        analysis['spos'].append(match.fromx)
+                        analysis['epos'].append(match.tox)
+                        analysis['suggestions'].append(match.replacements)
+                    dict_of_items[item][user].append(comment['data'])
+                        #print (str(match)+' THE CORRECTION AND THE SUGGESTION')
+                    comment_dict['analysis']=analysis
+                    user_dict['comment']=comment_dict
+                    print(json.dumps(user_dict,indent=4,sort_keys=True))
+                    r.db('lagrammer').table('comments').insert(user_dict).run()
+                    #print(' \n\n '+str(r.db('lagrammer').table('comments').filter({'name':user}).run()))
+                    
+
+
+
+    @staticmethod
+    def print_the_analysis(dictionary):
+        print(json.dumps(dictionary,indent=4,sort_keys=True))
+
+    def analyze_comments_from_stdin(self,pipe):
+        self.pipe=pipe
+        print ('Starting the Processor')
+        r.connect('localhost', 28015).repl()
+        tool=language_check.LanguageTool('en-GB')
+        try:
+            r.db_create('lagrammer').run()
+        except RqlRuntimeError:
+            try:
+                r.db('lagrammer').table_create('comments').run()
+            except RqlRuntimeError:
+                print("The table aready exists")
         comments={}
         pc = PlagiarismChecker()
         while True:
@@ -50,6 +135,7 @@ class Processor:
 
                 comment_dict={}
                 comment_dict['data']=input_data
+                comment_dict['datetimestamp']=str(datetime.now())
                 print('The sentence is:'+input_data)
                 matches=tool.check(input_data)
                 analysis={'rule_id':[],'str':[],'category':[],'msg':[],'spos':[],'epos':[],'suggestions':[]}
@@ -62,9 +148,6 @@ class Processor:
                     analysis['spos'].append(match.fromx)
                     analysis['epos'].append(match.tox)
                     analysis['suggestions'].append(match.replacements)
-
-
-
                     print (str(match)+' THE CORRECTION AND THE SUGGESTION')
                 
                 comment_dict['analysis']=analysis
