3b4f9a4a58eb364389761e5aeddb1d536b7c67ed
snehalbhayani <snehal.bhayani@yahoo.com>
2015-05-06 13:27:05 +0530

    Update test.py

--- grammarcheck/test.py
+++ grammarcheck/test.py
@@ -3,7 +3,7 @@ from processor import Processor
 p = Processor()
 #ld=p.process_things('test.dict')
 #ld=p.process_things('4.0.dict')
-p.start(None)
-
+p.analyze_comments_from_db(None)
+#p.start(None)
 
 
