06e4a3e372be9c9edad78b56029325bf50245cf0
Snehal <snehal.bhayani@yahoo.com>
2015-05-12 11:26:29 +0530

    Experimenting again

--- grammarcheck/test.py
+++ grammarcheck/test.py
@@ -3,14 +3,9 @@ from processor import Processor
 p = Processor()
 #ld=p.process_things('test.dict')
 #ld=p.process_things('4.0.dict')
-<<<<<<< HEAD
 #p.analyze_comments_from_db(None)
 #Processor.get_analysis()
 p.analyze_comments_from_stdin(None)
 
 
 
-=======
-p.analyze_comments_from_db(None)
-#p.analyze_comments_from_stdin(None)
->>>>>>> bad74ec4d51c587a91159c92b4c3ebe3e11b260d
