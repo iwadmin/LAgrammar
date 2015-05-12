ec6c82d88a8a2dd630021d9c74351bc89c627aeb
Snehal <snehal.bhayani@yahoo.com>
2015-05-12 10:52:08 +0530

    simple experiments

--- grammarcheck/test.py
+++ grammarcheck/test.py
@@ -3,7 +3,9 @@ from processor import Processor
 p = Processor()
 #ld=p.process_things('test.dict')
 #ld=p.process_things('4.0.dict')
-p.start(None)
+#p.analyze_comments_from_db(None)
+#Processor.get_analysis()
+p.analyze_comments_from_stdin(None)
 
 
 
